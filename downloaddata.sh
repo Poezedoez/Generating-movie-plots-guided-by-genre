a0ea25bf284d857e814561a412b41e2f5613e21d
Max Bos <maxn.bos@gmail.com>
2019-10-01 11:10:29 +0200

    Added download of glove data

--- downloaddata.sh
+++ downloaddata.sh
@@ -1,7 +1,9 @@
 mkdir data
 
 wget https://github.com/davidsbatista/text-classification/raw/master/movies_genres.csv.bz2
+wget http://nlp.stanford.edu/data/glove.6B.zip
 
 bunzip2 movies_genres.csv.bz2
+unzip glove.6B.zip
 
 mv movies_genres.csv data/
